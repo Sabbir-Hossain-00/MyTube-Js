d19a3767f1c94961c2cb364e378cfc70e0099753
Sabbir-Hossain-00 <ms7398037@gmail.com>
2025-03-12 21:29:57 +0600

    added some js function

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,107 @@
+const catagoryUrl = `https://openapi.programming-hero.com/api/phero-tube/categories`;
 const getCatagory = async ()=>{
-const url = `https://openapi.programming-hero.com/api/phero-tube/categories`
-const response = await fetch(url);
-const data = await response.json()
-const {categories} = data ;
-displayCatagory(categories);
+    const response = await fetch(catagoryUrl);
+    const data = await response.json()
+    const {categories} = data ;
+    displayCatagory(categories);
 }
+
+
+
+
+const getVideos = async (value)=>{
+  const videoUrl = `https://openapi.programming-hero.com/api/phero-tube/videos?title=${value}`;
+    const response = await fetch(videoUrl);
+    const data = await response.json()
+    const {videos} = data ;
+    displayVideo(videos);
+    
+}
+
+
+
+const catagoryVideo = async (id)=>{
+  const catVideoUrl = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+  const response = await fetch(catVideoUrl);
+  const data = await response.json();
+  console.log(data);
+  const {category} = data ;
+  displayVideo(category);
+}
+
+// const getSearchVideo = async (value)=>{
+//   const searchUrl = `https://openapi.programming-hero.com/api/phero-tube/videos?title=${value}`;
+//   const response = await fetch(searchUrl);
+//   const data = await response.json();
+//   const {videos} = data ;
+//   displayVideo(videos);
+// }
+
+
 
 const displayCatagory = (catagories)=>{
   for(const cat of catagories){
    const {category_id , category} = cat;
-   console.log(category_id , category)
    const newDiv = document.createElement("div");
    newDiv.innerHTML = `
-   <button class="btn btn-sm md:btn-md  hover:bg-[#FF1F3D] hover:text-white">${category}</button>
+   <button onclick = "catagoryVideo('${category_id}')" class="btn btn-sm md:btn-md  hover:bg-[#FF1F3D] hover:text-white">${category}</button>
    `;
    document.getElementById("catagory").append(newDiv)
   }
 }
+
+const displayVideo = (videos)=>{
+  const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+  for(const video of videos){
+    const {thumbnail , title , authors , others} = video ;
+    const [author] = authors ;
+    const {profile_picture , profile_name , verified} = author ;
+  
+    const newCard = document.createElement("div");
+    newCard.innerHTML = `
+    <div class="card bg-base-100 ">
+                <figure class="relative">
+                  <img 
+                    class="w-full h-[30vh] md:h-[20vh] xl:h-[30vh]"
+                    src="${thumbnail}" />
+                    <span class="absolute bottom-2 right-2 text-gray-700 text-sm">3hrs 56 min ago</span>
+                </figure>
+                <div class=" mt-3 flex justify-start items-start gap-3">
+                    <div class="avatar mt-1">
+                        <div class=" w-6 rounded-full">
+                          <img src="${profile_picture}" />
+                        </div>
+                      </div>
+                    <div class="space-y-2">
+                        <h2 class="card-title md:text-xl">${title}</h2> 
+                        <div class="flex justify-start items-center gap-2 ">
+                            <p class="text-sm text-gray-600">${profile_name}</p>
+                            
+                             <img class="w-4" src="assests/verified-badge-profile-icon-png.webp" alt="">
+                            
+                           
+                        </div>
+                        <p class="text-sm text-gray-600">${others.views}</p>
+                    </div>
+                  
+                </div>
+                <div class="card-actions ">
+                    <button class="btn btn-primary w-full mt-4">View Details</button>
+                  </div>
+              </div>
+    `
+    cardContainer.append(newCard);
+  }
+  
+}
+
+
+
+
+
 getCatagory();
+getVideos();
+document.getElementById("search-input").addEventListener("keyup",(e)=>{
+  getVideos(e.target.value);
+})
